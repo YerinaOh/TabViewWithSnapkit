dc301f237fb97160ea45800a84b289508e01b65f
YerinaOh <orina8262@gmail.com>
2019-04-15 11:31:41 +0900

    Initial Commit

---
+++ threeStructureTabView/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  threeStructureTabView
+//
+//  Created by OHYERINA on 15/04/2019.
+//  Copyright © 2019 OHYERINA. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ threeStructureTabView/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  threeStructureTabView
+//
+//  Created by OHYERINA on 15/04/2019.
+//  Copyright © 2019 OHYERINA. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
